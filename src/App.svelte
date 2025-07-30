efd145e7fde192d92f61d7d6450daade9aa46cb2
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-07-30 17:38:41 +0000

    'https://en.wikipedia.org/wiki/Svelte' copied and adapted to 'lang=ts' to compile it

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
   import Counter from "./lib/Counter.svelte";
+  import TodoList from "./lib/TodoList.svelte";
 </script>
 
-<Counter />
+<table>
+  <tbody>
+    <tr>
+      <td>
+        <Counter />
+      </td>
+      <td>
+        <TodoList />
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<style>
+  table,
+  td {
+    border-style: none;
+  }
+  td {
+    padding: 0rem 6rem;
+  }
+</style>

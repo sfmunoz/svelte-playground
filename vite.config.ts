cfa564a805e6a7a4ee4932f2d916c20fbaa80f51
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2025-08-03 10:09:07 +0000

    'vite.config.ts' adapted to use 'vite-tsconfig-paths'

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./", // default = '/' ; it may be absolute: '/svelte-playground/'
-  plugins: [tailwindcss(), svelte()],
+  plugins: [tailwindcss(), svelte(), tsconfigPaths()],
 });
